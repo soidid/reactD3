84d3acfa970d945484009e094e73c38a994d25c6
soidid <soidid@gmail.com>
2015-04-09 18:57:54 +0800

    Piechart draft

--- src/js/components/App/App.jsx
+++ src/js/components/App/App.jsx
@@ -2,7 +2,7 @@
 
 var React = require('react/addons');
 var BarChart = require('../BarChart/BarChart.jsx');
-
+var PieChart = require('../PieChart/PieChart.jsx');
 // var AppStore = require('../../stores/AppStore');
 // var AppActions = require('../../actions/AppActions');
 require('./App.css');
@@ -11,7 +11,8 @@ var App = React.createClass({
 
   getInitialState(){
     return {
-      data: []
+      data: [],
+      pieData: []
     }
   },
   
@@ -21,30 +22,42 @@ var App = React.createClass({
  
   _onRandomData(){
     var newArray = [];
+    var newArrayPie = [];
+
+
     for(var i=0;i<5;i++){
-       // 產生 0-9 的隨機數字
-       var value = Math.floor(Math.random() * 10);
-       var label = "item"+i;
-       newArray.push({
-           label : label,
-           value : value
-       });
-    } 
+      // 產生 0-9 的隨機數字
+      var value = Math.floor(Math.random() * 10);
+      var label = "item"+i;
+      newArray.push({
+          label : label,
+          value : value
+      });
+
+      newArrayPie.push({
+          text: label,
+          quantity : value
+      }); 
+    }
+
     this.setState({
-       data: newArray
+       data: newArray,
+       pieData: newArrayPie
     });
     
   },
 
   render () {
-    var data = this.state.data;
+    var { data, pieData } = this.state;
+    var colorRange = ["#0b64a0", "#5098d8", "#80b2e0", "#afcfef", "#d4e6f9",  "#fcedd6", "#f7e3bf", "#fcce65", "#fec92d", "#f4b425"];
+
     var tooltip = {
         "hidden" : true,
         "top" : "10px",
         "left" : "100px",
         "html" : "hello"
         };
-
+    console.log(pieData[0]);
     return (
       <div className="App">
 
@@ -53,6 +66,9 @@ var App = React.createClass({
                   data={data}
                   tooltip={tooltip} />
         <div><button onClick={this._onRandomData}>Change!</button></div>
+        <br/>
+
+        <PieChart colorRange={colorRange} data={pieData} width={300} height={300} />
       </div>
     );
   }
---
+++ src/js/components/PieChart/PieChart.jsx
@@ -0,0 +1,102 @@
+/** @jsx React.DOM */
+var React = require('react/addons');
+var d3 = require("d3");
+var Chart = require('../Chart/Chart.jsx');
+
+var PieChart = React.createClass({
+
+  propTypes: {
+    colorRange: React.PropTypes.array.isRequired
+  }, 
+
+  getInitialState() {
+    return {
+      colorRange: this.props.colorRange,
+      arcRadius: Math.min(this.props.width, this.props.height) / 2,
+    };
+  },
+  
+  _me: null,
+
+  _color() {
+    var rangeData = [];
+    var dataLength = this.props.data.length;
+    var colorLength = this.state.colorRange.length;
+    for(var i = 0; i < colorLength; i++){
+      rangeData.push(dataLength * i / colorLength);
+    }
+    return d3.scale.linear()
+    .domain(rangeData)
+      .range(this.state.colorRange); 
+  },
+
+  _arc() {
+    return d3.svg.arc()
+      .outerRadius(this.state.arcRadius - 10)
+      .innerRadius(this.state.arcRadius/2);
+  },
+
+  _pie(data) {
+    return d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.quantity; }).call(null,data);
+  },
+
+  _renderGraph () {
+    var _this = this;
+    var data = this.props.data;
+    
+    
+    // Based in http://bl.ocks.org/mbostock/3887235
+    //if(!this._me){ 
+      this._me = d3.select(this.getDOMNode()).append('g')
+      .attr("transform", "translate(" +
+        this.props.width / 2 + "," + this.props.height / 2 + ")"
+      ); 
+    //}
+
+    var g = this._me.selectAll(".arc")
+      .data(this._pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+    g.append("path")
+      .attr("d", this._arc())
+      .style("fill", function(d, i) { 
+        return _this._color().call(null, i);
+      });
+
+    g.append("text")
+      .attr("transform", function(d) {
+        return "translate(" + _this._arc().centroid(d) + ")";
+      })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.text; });
+  },
+
+  componentDidMount () {
+    this._renderGraph();
+  },
+
+  componentDidUpdate () {
+    console.log("component update!");
+    this._renderGraph();
+  },
+
+  // shouldComponentUpdate (nextProps) {
+  //   this._renderGraph();
+  //   return false;
+  // },
+
+  render() {
+    return (
+      <svg width={this.props.width} height={this.props.height}></svg>
+    );
+  }
+});
+
+
+module.exports = PieChart;
+
+
